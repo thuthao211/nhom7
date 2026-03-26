3bdc253fa461287943d18f176fb5489187978ebb
ltphuonganh <030239230007@st.buh.edu.vn>
2026-03-26 20:36:25 +0700

    Bo sung trang chi tiet sach

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViduLayoutController;
-
+use App\Http\Controllers\SachController; 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/trang1', [ViduLayoutController::class, 'trang1']);
 Route::get('/sach', [ViduLayoutController::class, 'sach']);
+
+Route::get('/chitietsach/{id}', [SachController::class, 'chitietsach']);
