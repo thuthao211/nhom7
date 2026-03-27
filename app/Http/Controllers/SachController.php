c24d780d79f2c1c97e5512cc40d0bcf452963b66
ThachThao-gif <030239230215@st.buh.edu.vn>
2026-03-27 16:02:30 +0700

    updated final

--- app/Http/Controllers/SachController.php
+++ app/Http/Controllers/SachController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class SachController extends Controller
 {
-
     function chitietsach($id) {
     $data2 = DB::table('sach')->where('id', $id)->get();
     return view('components.chitietsach', compact('data2'));
